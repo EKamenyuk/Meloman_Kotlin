aba7abc5db516acb9290f2a7879e6a572d310b91
EKamenyuk <e.kamenyuk@mail.ru>
2022-08-15 21:08:19 +0700

    "Вторая редакция, исправление №1"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,28 @@
 fun main() {
+    val discountStart1 = 1_001_00
+    val discountStart2 = 10_001_00
+    val discount1 = 100_00
+    val discount2 = 0.05
+    val discount3 = 0.01
 
-    val purchase = 15_000  // вводимая величина
-    val userMeloman = "yes"  // вводиться либо yes - постоянный клиент либо no - непостоянный
-    val discount1 = 100
-    val discount2 = 95
-    val discount3 = 1
-    val discountStart1 = 1_001
-    val discountStart2 = 10_001
-    var totalPrace = purchase
-    val result = if (purchase > discountStart1 && purchase < discountStart2 && userMeloman != "yes") {
-        totalPrace = purchase - discount1
-    } else if (purchase > discountStart1 && purchase < discountStart2 && userMeloman == "yes") {
-        totalPrace = purchase - discount1 - purchase * discount3 / 100
-    } else if (purchase > discountStart2 && userMeloman != "yes") {
-        totalPrace = purchase * discount2 / 100
+    var purchaseValue = 320_00            // сумма покупки
+    val amountPreviousPurchase = 1850_00  // предыдущая сумма покупки в копейках
+    val userMeloman = true
+
+    val intermediateDiscount: Int
+    if (amountPreviousPurchase > discountStart1 && amountPreviousPurchase <= discountStart2) {
+        intermediateDiscount = purchaseValue - discount1
+    } else if (amountPreviousPurchase > discountStart2) {
+        intermediateDiscount = (purchaseValue * (1 - discount2)).toInt()
+    } else intermediateDiscount = purchaseValue
+
+    val finalDiscount: Int
+    if (userMeloman) {
+        finalDiscount = (intermediateDiscount * (1 - discount3)).toInt()
     } else {
-        totalPrace = purchase * discount2 / 100 - purchase * discount3 / 100
+        finalDiscount = intermediateDiscount
     }
-    println()
-    println(totalPrace)
+    println("покупка - " + purchaseValue / 100 + "." + purchaseValue % 100)
+    println("после применения скидки - " + intermediateDiscount / 100 + "." + intermediateDiscount % 100)
+    println("после применения 1% скидки - " + finalDiscount / 100 + "." + finalDiscount % 100)
 }
